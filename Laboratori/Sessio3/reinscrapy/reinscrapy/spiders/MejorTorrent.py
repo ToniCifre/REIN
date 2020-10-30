3d794f2ef856ff61350793f3027529adfe4e822f
tonix <tonicifrev@gmail.com>
2020-10-30 11:14:30 +0100

    Scrapy updated

--- Laboratori/Sessio3/reinscrapy/reinscrapy/spiders/MejorTorrent.py
+++ Laboratori/Sessio3/reinscrapy/reinscrapy/spiders/MejorTorrent.py
@@ -5,8 +5,8 @@ from scrapy.selector import Selector
 class MejortorrentSpider(scrapy.Spider):
     name = 'MejorTorrent'
     allowed_domains = ['mejortorrentt.net']
-    start_urls = ['http://www.mejortorrentt.net/torrents-de-peliculas.html']
     n_page = 1
+    start_urls = [f'http://www.mejortorrentt.net/secciones.php?sec=descargas&ap=peliculas&p={n_page}']
 
     def parse(self, response):
         """
@@ -15,17 +15,19 @@ class MejortorrentSpider(scrapy.Spider):
         :param response:
         :return:
         """
-
+        print(f'[INFO]: Number of page {self.n_page}')
         for box in response.css('td.main_table_center_content_td'):
             for films in box.css('a::attr(href)').getall():
-                doc = {}
                 link = response.urljoin(films)
-                yield scrapy.Request(link, callback=self.parse_film_detail, meta=doc)
+                yield scrapy.Request(link, callback=self.parse_film_detail)
 
-        if self.n_page <= 50:
+        if int(response.request.url.split('=')[-1]) == self.n_page:
             self.n_page += 1
             next_page = response.urljoin(f'secciones.php?sec=descargas&ap=peliculas&p={self.n_page}')
             yield scrapy.Request(next_page, callback=self.parse)
+        else:
+            print('-----------------------------------')
+            print(response.request.url.split('='), self.n_page)
 
     def parse_film_detail(self, response):
         """
@@ -34,27 +36,27 @@ class MejortorrentSpider(scrapy.Spider):
         :param response:
         :return:
         """
+        doc = {}
         sel = Selector(response).xpath('//*[@id="main_table_center_center1"]/table/tr/td[2]/table/tr/td[1]')
 
-        aux = [str(k).replace('\n', '').replace('\t', '').replace('\r', '').lstrip() for k in
-               sel.css('*::text').getall()]
+        aux = [str(k).replace('\n', '').replace('\t', '').replace('\r', '').lstrip()
+               for k in sel.css('*::text').getall()]
         aux = [k for k in aux if k != '']
 
         info_list = ['Género', 'Año', 'Director', 'Actores', 'Formato',
                      'Total Descargas', 'Fecha', 'Tamaño', 'Descripción']
         info_title = 'title'
-        doc = response.meta
-        for t in aux:
+        for data in aux:
             if info_title != '':
-                doc[info_title] = t
+                doc[info_title] = data
                 info_title = ''
-            elif ':' in t:
-                info_title = t.replace(':', '').replace(' ', '')
+            elif ':' in data:
+                info_title = data.replace(':', '').replace(' ', '')
                 if info_title not in info_list:
                     info_title = ''
 
-        yield scrapy.Request(response.urljoin(sel.css('a::attr(href)').getall()[0]), callback=self.parse_torrent,
-                             meta=doc)
+        yield scrapy.Request(response.urljoin(sel.css('a::attr(href)').getall()[0]),
+                             callback=self.parse_torrent, meta=doc)
 
     def parse_torrent(self, response):
         """
@@ -64,9 +66,10 @@ class MejortorrentSpider(scrapy.Spider):
         :return:
         """
         detail = response.meta
-
         sel = Selector(response).xpath('//*[@id="contenido_descarga"]/table/tr/td[1]/table/tr/td[1]')
-        for link in sel.css('a::attr(href)').getall():
-            detail['download'] = response.urljoin(str(link))
+
+        link = sel.css('a::attr(href)').getall()
+        if len(link) != 0:
+            detail['download'] = response.urljoin(str(link[0]))
 
         yield detail
